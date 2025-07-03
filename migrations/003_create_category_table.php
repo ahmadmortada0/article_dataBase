a433099eca9ceef9c41e453be296432682e838df
Ahmed Mourtada <moudi_2358@hotmail.com>
2025-07-03 22:40:35 +0300

    creating a create file for catogry

---
+++ migrations/003_create_category_table.php
@@ -0,0 +1,11 @@
+<?php
+    require ("../connection/connection.php");
+
+    $query="CREATE TABLE category
+             id int(11) AUTO_INCREMENT PRIMARY KEY,
+             title VARCHAR(255) NOT NULL,
+             description  TEXT NOT NULL , 
+             price int(11) NOT NULL
+    ";
+    $excute = $mysqli->prepare($query);
+    $excute = $mysql->excute();
